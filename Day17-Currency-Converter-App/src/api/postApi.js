1663746cfc202bd79d3b459ebb40d3829224a47b
Sanket Malvi <sanketmalvi5@gmail.com>
2025-07-03 14:33:51 +0530

    Updated code

--- Day17-Currency-Converter-App/src/api/postApi.js
+++ Day17-Currency-Converter-App/src/api/postApi.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
+const API_KEY =import.meta.env.REACT_APP_EXCHANGE_RATE_API_KEY;
+
 const api = axios.create({
-  baseURL: "https://v6.exchangerate-api.com/v6/dc1b5600e649bf3cdf3a2661", 
+  baseURL: `https://v6.exchangerate-api.com/v6/${API_KEY}`, 
 });
 
 export const currencyConverter = (fromCurrency, toCurrency, amount) => {
